577fcb3d7b318c5e3531642ab16b749cf6cedbb1
nobuyo <world_fantasy_2017@live.jp>
2017-02-03 13:06:21 +0900

    rename

--- db/migrate/20170203031608_create_aspire_univs.rb
+++ db/migrate/20170203031608_create_aspire_univs.rb
@@ -1,4 +1,4 @@
-class CreateAspireunivs < ActiveRecord::Migration[5.0]
+class CreateAspireUnivs < ActiveRecord::Migration[5.0]
   def change
     create_table :aspire_univs do |t|
       t.reference :user
