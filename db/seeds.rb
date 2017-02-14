9531377bc59c9cc142e8a2c33d1a502e4bc2fe47
nobuyo <world_fantasy_2017@live.jp>
2017-02-14 17:01:44 +0900

    update db/seeds.rb, etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
 require "date"
 # create admin
-admin = User.new(email: 'admin@hendb.com', is_admin: true)
-admin.encrypt_password('password')
-admin.save!
+# admin = User.new(email: 'admin@hendb.com', is_admin: true)
+# admin.encrypt_password('password')
+# admin.save!
 
 s1 = Date.parse("2017/02/03")
 s2 = Date.parse("2017/12/31")
 
 (1..10).each do |a|
-  univ = Univ.new(name: "Univ#{a}",
+  univ = Univ.new(
+    name: "Univ#{a}",
     dept: "Dept#{a}",
     pref: "Pref#{a}",
-    deviation_value: Random.rand(35..65),
+    deviation_value: 60 - a,
     exam_date: Random.rand(s1 .. s2),
     result_date: Random.rand(s1 .. s2),
     affirmation_date: Random.rand(s1 .. s2),
     document_url: 'https://github.com',
-    remark: "univ univ univ")
-    3.times do
-      univ.exams.build(subject: Random.rand(1..9))
-    end
-    univ.save!
+    remark: "備考です")
+  sbj = (1..9).to_a.shuffle!
+  3.times do
+    univ.exams.build(subject: sbj.pop)
+  end
+  univ.save!
 end
